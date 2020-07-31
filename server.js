df0fa9e1e87891711323e8e700e8e5909d3d1535
Keith Beaudoin <bowdwin.muninted@gmail.com>
2020-07-31 13:34:05 -0400

    added mongo url to server.js

--- server.js
+++ server.js
@@ -1,27 +1,29 @@
-const express = require("express");
-const logger = require("morgan");
-const mongoose = require("mongoose");
-const compression = require("compression");
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const compression = require('compression');
 
 const PORT = 3000;
 
 const app = express();
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-mongoose.connect("mongodb://localhost/budget", {
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/budget';
+
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require('./routes/api.js'));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
---
+++ startup.md
@@ -0,0 +1,9 @@
+## Action Items
+
+    * create an app manifest
+        - public/manfest/webmanifest
+    *Register and install a service worker
+        -public/server-worker.js
+    * carch our files to deliver offline experience
+    * make the app downloadable
+    * Utilize background sync to enable Sync of offline data when going back online
